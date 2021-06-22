3e7f7cd3c94994fdd8c7487932a9e59ff37cb10d
realisticattorney <g.axl.aquila@gmail.com>
2021-06-21 21:14:55 -0300

    Add request, success and failure cases to reducer

--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,30 +1,34 @@
 /* eslint-disable */
-const initialState = [];
-
-
+const initialState = {
+  stocks: [],
+  loading: false,
+  error: null,
+};
 
 const stocksReducer = (state = initialState, action) => {
-  console.log(state)
+  console.log(state);
   switch (action.type) {
-    case 'FETCH_STOCKS':
-        return action.payload
-  //     return {
-  //       ...state,
-  //       stocks: action.payload,
-  //     };
-  //   case 'FILTER':
-  //     return {
-  //       ...state,
-  //       filter: action.filter,
-  //     };
-  //   case 'LOADING':
-  //     return {
-  //       ...state,
-  //       loading: action.state,
-  //     };
+    case 'FETCH_STOCKS_REQUEST':
+      return {
+        ...state,
+        loading: true,
+        error: null,
+      };
+    case 'FETCH_STOCKS_SUCCESS':
+      return {
+        ...state,
+        loading: false,
+        stocks: action.payload,
+      };
+    case 'FETCH_STOCKS_FAILURE':
+      return {
+        ...state,
+        loading: false,
+        stocks: action.error,
+      };
     default:
-      return state ;
+      return state;
   }
-}
+};
 
 export default stocksReducer;
